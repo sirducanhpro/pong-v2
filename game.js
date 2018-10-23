15404ebca6805bc63e6c6caf2f7359534c95c344
sirducanhpro <sirducanhpro@gmail.com>
2018-10-22 20:38:41 -0400

    Pong game

--- game.js
+++ game.js
@@ -4,6 +4,47 @@ var sketchProc = function(processingInstance) {
         size(400, 400); 
        
         // Program goes here
+       //
+//Pong game
+//
+//This is a game where you try to keep the ball from exiting the screen using a paddle
+//
+//author : Anh Cuto
+//Date: 9/12/2018
+
+//start of the ball in the center of the screen
+
+
+
+var ballX = 200, ballY = 200;
+var ballXVel = 3, ballYVel = -2;
+var ballDiameter = 20;
+var ballRadius = ballDiameter / 2;
+var isGamePaused=false;
+
+var paddleA=150;
+var paddleB=150;
+
+var ballColor=20;
+
+var scoreLeft=0;
+var scoreRight=0;
+
+//make the bounce off the wall
+var makeBallBounce = function(){
+
+
+
+
+if (ballY - ballRadius <= 0){
+    ballYVel= -ballYVel;   
+}
+if (ballY + ballRadius >=400){
+    ballYVel= -ballYVel;   
+}
+}; 
+
+
         draw = function() {
             if (pmouseX > 0) {
                 line(mouseX, mouseY, pmouseX, pmouseY);
