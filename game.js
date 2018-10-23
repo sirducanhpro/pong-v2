709160d792c8b9f86fff9a6236085f9ba6629b0b
sirducanhpro <sirducanhpro@gmail.com>
2018-10-23 08:45:41 -0400

    pong

--- game.js
+++ game.js
@@ -45,10 +45,110 @@ if (ballY + ballRadius >=400){
 }; 
 
 
-        draw = function() {
-            if (pmouseX > 0) {
-                line(mouseX, mouseY, pmouseX, pmouseY);
-            }
-        }
+draw = function() {
+    
+ 
+if(isGamePaused) {
+    return;
+}
+    background(176, 254, 255);
+
+//losing conditions (if ball hit left or right edges)
+if(ballX - ballRadius <= 0 || ballX + ballRadius >= width ){
+    isGamePaused= true;  
+    fill(153, 38, 166);
+    textSize(40);
+    
+    var f= createFont("calibri");
+    textFont(f);
+    
+    text("YOU LOST",117,38);
+    textSize(15);
+    text("Press r to play again",139,70);
+    
+    
+    //increase score 
+    if(ballX - ballRadius <= 0){
+        scoreRight++;
+    }
+    if( ballX + ballRadius >= width){
+        scoreLeft++;
+    }
+
+}
+//ball color
+    colorMode(HSB);
+    noStroke();
+    fill(ballColor, 159, 199);
+    colorMode(RGB);
+    
+//ball color changing
+    ballColor+=1;
+    ballColor= ballColor % 255;
+    
+//make ball move  
+    ballX+=ballXVel;
+    ballY+=ballYVel;
+    
+//make ball bounce    
+    makeBallBounce();
+  
+ //draw all the shapes  
+    ellipse(ballX, ballY, ballDiameter, ballDiameter);
+    
+    fill(61, 55, 55);
+    rect(382,paddleA,10,100,5);
+    rect(8,paddleB,10,100,5);
+    
+    
+    
+    
+    //score display
+    text(scoreLeft,13,16);
+    text(scoreRight,378,16);
+    
+//check if ball hit the paddle
+    if(ballX- ballRadius <=18 && ballY<=paddleB+96 && ballY>=paddleB){
+        ballXVel = abs(ballXVel);
+    }
+    if(ballX+ ballRadius >=382 && ballY<=paddleA+96 && ballY>=paddleA){
+        ballXVel = -abs(ballXVel);
+    }
+    
+//move the paddles
+    if(keyIsPressed && keyCode === UP && paddleA>=0){
+        paddleA-=3;   
+    }
+    if(keyIsPressed && keyCode === DOWN && paddleA <=300){
+        paddleA+=3;   
+    }
+    if(keyIsPressed && key.toString() === 'w' && paddleB >=0){
+        paddleB-=3;   
+    }
+    if(keyIsPressed && key.toString() === 's' && paddleB <=300){
+        paddleB+=3;   
+    }
+};
+
+
+//create interaction to game    
+var keyPressed=function(){
+//restart the game
+if(key.toString() === 'r'){
+    isGamePaused=false;
+    ballX=200;
+    ballY=200;
+    paddleA=150;
+    paddleB=150;
+}
+
+//change position of right paddle
+
+
+//min max
+
+};
+frameRate(60);
+
     }
 };
